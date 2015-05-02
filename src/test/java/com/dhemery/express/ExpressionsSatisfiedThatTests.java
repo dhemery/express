9417231f088d44a559f3e2199a0e3cdebbe7d6a9
Dale Emery <dale@dhemery.com>
2015-05-02 14:15:49 -0700

    Simplify ExpressionsSatisfiedThatTests

--- src/test/java/com/dhemery/express/ExpressionsSatisfiedThatTests.java
+++ src/test/java/com/dhemery/express/ExpressionsSatisfiedThatTests.java
@@ -3,9 +3,13 @@ package com.dhemery.express;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
 
 public class ExpressionsSatisfiedThatTests {
+    private static final String SUBJECT = "foo";
+    private static final String FUNCTION_VALUE = "bar";
+
     @Test
     public void withBooleanSupplier_returnsTrue_ifSupplierReturnsTrue() {
         boolean result = Expressions.satisfiedThat(() -> true);
@@ -20,49 +24,49 @@ public class ExpressionsSatisfiedThatTests {
 
     @Test
     public void withSubjectPredicate_returnsTrue_ifPredicateAcceptsSubject() {
-        boolean result = Expressions.satisfiedThat("", String::isEmpty);
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> true);
         assertThat(result, is(true));
     }
 
     @Test
     public void withSubjectPredicate_returnsFalse_ifPredicateRejectsSubject() {
-        boolean result = Expressions.satisfiedThat("foo", String::isEmpty);
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> false);
         assertThat(result, is(false));
     }
 
     @Test
     public void withSubjectMatcher_returnsTrue_ifMatcherAcceptsSubject() {
-        boolean result = Expressions.satisfiedThat("foo", is("foo"));
+        boolean result = Expressions.satisfiedThat(SUBJECT, equalTo(SUBJECT));
         assertThat(result, is(true));
     }
 
     @Test
     public void withSubjectMatcher_returnsFalse_ifMatcherRejectsSubject() {
-        boolean result = Expressions.satisfiedThat("foo", is("food"));
+        boolean result = Expressions.satisfiedThat(SUBJECT, equalTo(SUBJECT + "!"));
         assertThat(result, is(false));
     }
 
     @Test
     public void withSubjectFunctionMatcher_returnsTrue_ifMatcherAcceptsFunctionOfSubject() {
-        boolean result = Expressions.satisfiedThat("foo", String::toUpperCase, is("FOO"));
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> FUNCTION_VALUE, equalTo(FUNCTION_VALUE));
         assertThat(result, is(true));
     }
 
     @Test
     public void withSubjectFunctionMatcher_returnsFalse_ifMatcherRejectsFunctionOfSubject() {
-        boolean result = Expressions.satisfiedThat("foo", String::toUpperCase, is("foo"));
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> FUNCTION_VALUE, equalTo(FUNCTION_VALUE + "!"));
         assertThat(result, is(false));
     }
 
     @Test
     public void withSubjectFunctionPredicate_returnsTrue_ifPredicateAcceptsFunctionOfSubject() {
-        boolean result = Expressions.satisfiedThat("foo", String::toUpperCase, "FOO"::equals);
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> FUNCTION_VALUE, s -> true);
         assertThat(result, is(true));
     }
 
     @Test
     public void withSubjectFunctionPredicate_returnsFalse_ifPredicateRejectsFunctionOfSubject() {
-        boolean result = Expressions.satisfiedThat("foo", String::toUpperCase, "foo"::equals);
+        boolean result = Expressions.satisfiedThat(SUBJECT, s -> FUNCTION_VALUE, s -> false);
         assertThat(result, is(false));
     }
 }
