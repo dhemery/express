a5e3e42f6ffaf2d55aae00feac820cdd70c381a4
Dale Emery <dale@dhemery.com>
2015-05-03 15:55:10 -0700

    Organize SubjectMatcherExpressionTests into inner classes

--- src/test/java/com/dhemery/express/SubjectMatcherExpressionTests.java
+++ src/test/java/com/dhemery/express/SubjectMatcherExpressionTests.java
@@ -1,53 +1,45 @@
 package com.dhemery.express;
 
 import com.dhemery.express.helpers.Throwables;
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.SelfDescribing;
-import org.jmock.Expectations;
-import org.jmock.auto.Mock;
-import org.jmock.integration.junit4.JUnitRuleMockery;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 
-import static com.dhemery.express.helpers.Actions.appendItsStringValue;
-import static com.dhemery.express.helpers.Actions.appendTheMismatchDescriptionOfTheItem;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
-// TODO: Package into nested classes
 public class SubjectMatcherExpressionTests {
-    private static final String SUBJECT = "subject";
 
-    @Rule public JUnitRuleMockery context = new JUnitRuleMockery();
+    public static class AssertThat {
+        @Test
+        public void returnsWithoutThrowing_ifMatcherAcceptsSubject() {
+            Expressions.assertThat("subject", anything());
+        }
 
-    @Test
-    public void assertThat_returnsWithoutThrowing_ifMatcherAcceptsSubject() {
-        Expressions.assertThat(SUBJECT, anything());
+        @Test(expected = AssertionError.class)
+        public void throwsAssertionError_ifMatcherRejectsSubject() {
+            Expressions.assertThat("subject", not(anything()));
+        }
+
+        @Test
+        public void errorMessage_describesMatcherAndMismatch() {
+            String message = Throwables.messageThrownBy(() -> Expressions.assertThat("subject", not(anything())));
+
+            assertThat(message, is(Diagnosis.of("subject", not(anything()))));
+        }
     }
 
-    @Test(expected = AssertionError.class)
-    public void assertThat_throwsAssertionError_ifMatcherRejectsSubject() {
-        Expressions.assertThat(SUBJECT, not(anything()));
-    }
+    public static class SatisfiedThat {
+        @Test
+        public void returnsTrue_ifMatcherAcceptsSubject() {
+            boolean result = Expressions.satisfiedThat("subject", anything());
 
-    @Test
-    public void assertThat_errorMessageIncludesDiagnosis() {
-        String message = Throwables.messageThrownBy(() -> Expressions.assertThat(SUBJECT, not(anything())));
+            assertThat(result, is(true));
+        }
 
-        assertThat(message, is(Diagnosis.of(SUBJECT, not(anything()))));
-    }
+        @Test
+        public void returnsFalse_ifMatcherRejectsSubject() {
+            boolean result = Expressions.satisfiedThat("subject", not(anything()));
 
-    @Test
-    public void satisfiedThat_returnsTrue_ifMatcherAcceptsSubject() {
-        boolean result = Expressions.satisfiedThat(SUBJECT, anything());
-        assertThat(result, is(true));
-    }
-
-    @Test
-    public void satisfiedThat_returnsFalse_ifMatcherRejectsSubject() {
-        boolean result = Expressions.satisfiedThat(SUBJECT, not(anything()));
-        assertThat(result, is(false));
+            assertThat(result, is(false));
+        }
     }
 }
