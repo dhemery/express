65c11d344cde6f82916ca0cea67955818d8332f8
Dale Emery <dale@dhemery.com>
2015-05-03 17:29:52 -0700

    Move when(subject, predicate) tests into SubjectPredicatePolledExpressionsTests

--- src/test/java/com/dhemery/express/SubjectPredicatePolledExpressionTests.java
+++ src/test/java/com/dhemery/express/SubjectPredicatePolledExpressionTests.java
@@ -1,25 +1,22 @@
 package com.dhemery.express;
 
-import com.dhemery.express.helpers.ExpressionsPolledBy;
 import com.dhemery.express.helpers.PolledExpressionTestSetup;
 import com.dhemery.express.helpers.PollingSchedules;
-import org.hamcrest.SelfDescribing;
 import org.jmock.Expectations;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 
-import java.time.Duration;
-import java.util.function.Predicate;
-
 import static com.dhemery.express.helpers.Throwables.messageThrownBy;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
 
 @RunWith(Enclosed.class)
 public class SubjectPredicatePolledExpressionTests {
     public static final SelfDescribingPredicate<String> PREDICATE = Named.predicate("predicate", t -> true);
+    public static final String SUBJECT = "subject";
 
     public static class AssertThat extends PolledExpressionTestSetup {
         PollingSchedule schedule = PollingSchedules.random();
@@ -27,33 +24,33 @@ public class SubjectPredicatePolledExpressionTests {
         @Test
         public void returnsWithoutThrowing_ifPollReturnsTrue() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(true));
             }});
 
-            expressions.assertThat(schedule, "subject", PREDICATE);
+            expressions.assertThat(schedule, SUBJECT, PREDICATE);
         }
 
         @Test(expected = AssertionError.class)
         public void throwsAssertionError_ifPollReturnsFalse() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            expressions.assertThat(schedule, "subject", PREDICATE);
+            expressions.assertThat(schedule, SUBJECT, PREDICATE);
         }
 
         @Test
         public void errorMessage_describesSubjectPredicateAndSchedule() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            String message = messageThrownBy(() -> expressions.assertThat(schedule, "subject", PREDICATE));
+            String message = messageThrownBy(() -> expressions.assertThat(schedule, SUBJECT, PREDICATE));
 
-            assertThat(message, is(Diagnosis.of(schedule, "subject", PREDICATE)));
+            assertThat(message, is(Diagnosis.of(schedule, SUBJECT, PREDICATE)));
         }
     }
 
@@ -63,11 +60,11 @@ public class SubjectPredicatePolledExpressionTests {
         @Test
         public void returnsTrue_ifPollReturnsTrue() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(true));
             }});
 
-            boolean result = expressions.satisfiedThat(schedule, "subject", PREDICATE);
+            boolean result = expressions.satisfiedThat(schedule, SUBJECT, PREDICATE);
 
             assertThat(result, is(true));
         }
@@ -75,11 +72,11 @@ public class SubjectPredicatePolledExpressionTests {
         @Test
         public void returnsFalse_ifPollReturnsFalse() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            boolean result = expressions.satisfiedThat(schedule, "subject", PREDICATE);
+            boolean result = expressions.satisfiedThat(schedule, SUBJECT, PREDICATE);
 
             assertThat(result, is(false));
         }
@@ -96,33 +93,33 @@ public class SubjectPredicatePolledExpressionTests {
         @Test
         public void returnsWithoutThrowing_ifPollReturnsTrue() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(defaultSchedule, "subject", PREDICATE);
+                allowing(poller).poll(defaultSchedule, SUBJECT, PREDICATE);
                 will(returnValue(true));
             }});
 
-            expressions.waitUntil("subject", PREDICATE);
+            expressions.waitUntil(SUBJECT, PREDICATE);
         }
 
         @Test(expected = PollTimeoutException.class)
         public void throwsPollTimeoutException_ifPollReturnsFalse() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(defaultSchedule, "subject", PREDICATE);
+                allowing(poller).poll(defaultSchedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            expressions.waitUntil("subject", PREDICATE);
+            expressions.waitUntil(SUBJECT, PREDICATE);
         }
 
         @Test
         public void exceptionMessage_describesSubjectPredicateAndSchedule() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(expressions.eventually(), "subject", PREDICATE);
+                allowing(poller).poll(expressions.eventually(), SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            String message = messageThrownBy(() -> expressions.waitUntil("subject", PREDICATE));
+            String message = messageThrownBy(() -> expressions.waitUntil(SUBJECT, PREDICATE));
 
-            assertThat(message, is(Diagnosis.of(expressions.eventually(), "subject", PREDICATE)));
+            assertThat(message, is(Diagnosis.of(expressions.eventually(), SUBJECT, PREDICATE)));
         }
     }
 
@@ -132,33 +129,112 @@ public class SubjectPredicatePolledExpressionTests {
         @Test
         public void returnsWithoutThrowing_ifPollReturnsTrue() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(true));
             }});
 
-            expressions.waitUntil(schedule, "subject", PREDICATE);
+            expressions.waitUntil(schedule, SUBJECT, PREDICATE);
         }
 
         @Test(expected = PollTimeoutException.class)
         public void throwsPollTimeoutException_ifPollReturnsFalse() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            expressions.waitUntil(schedule, "subject", PREDICATE);
+            expressions.waitUntil(schedule, SUBJECT, PREDICATE);
         }
 
         @Test
         public void exceptionMessage_describesSubjectPredicateAndSchedule() {
             context.checking(new Expectations() {{
-                allowing(poller).poll(schedule, "subject", PREDICATE);
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
                 will(returnValue(false));
             }});
 
-            String message = messageThrownBy(() -> expressions.waitUntil(schedule, "subject", PREDICATE));
+            String message = messageThrownBy(() -> expressions.waitUntil(schedule, SUBJECT, PREDICATE));
 
-            assertThat(message, is(Diagnosis.of(schedule, "subject", PREDICATE)));
+            assertThat(message, is(Diagnosis.of(schedule, SUBJECT, PREDICATE)));
+        }
+    }
+
+    public static class WhenWithDefaultPollingSchedule extends PolledExpressionTestSetup {
+        private PollingSchedule defaultSchedule;
+
+        @Before
+        public void setUp() {
+            defaultSchedule = expressions.eventually();
+        }
+
+        @Test
+        public void returnsSubject_ifPollReturnsTrue() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(defaultSchedule, SUBJECT, PREDICATE);
+                will(returnValue(true));
+            }});
+
+            String returnedValue = expressions.when(SUBJECT, PREDICATE);
+            assertThat(returnedValue, is(sameInstance(SUBJECT)));
+        }
+
+        @Test(expected = PollTimeoutException.class)
+        public void throwsPollTimeoutException_ifPollReturnsFalse() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(defaultSchedule, SUBJECT, PREDICATE);
+                will(returnValue(false));
+            }});
+
+            expressions.when(SUBJECT, PREDICATE);
+        }
+
+        @Test
+        public void exceptionMessage_describesSubjectPredicateAndSchedule() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(expressions.eventually(), SUBJECT, PREDICATE);
+                will(returnValue(false));
+            }});
+
+            String message = messageThrownBy(() -> expressions.when(SUBJECT, PREDICATE));
+
+            assertThat(message, is(Diagnosis.of(expressions.eventually(), SUBJECT, PREDICATE)));
+        }
+    }
+
+    public static class WhenWithExplicitPollingSchedule extends PolledExpressionTestSetup {
+        PollingSchedule schedule = PollingSchedules.random();
+
+        @Test
+        public void returnsSubject_ifPollReturnsTrue() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
+                will(returnValue(true));
+            }});
+
+            String returnedValue = expressions.when(schedule, SUBJECT, PREDICATE);
+            assertThat(returnedValue, is(sameInstance(SUBJECT)));
+        }
+
+        @Test(expected = PollTimeoutException.class)
+        public void throwsPollTimeoutException_ifPollReturnsFalse() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
+                will(returnValue(false));
+            }});
+
+            expressions.when(schedule, SUBJECT, PREDICATE);
+        }
+
+        @Test
+        public void exceptionMessage_describesSubjectPredicateAndSchedule() {
+            context.checking(new Expectations() {{
+                allowing(poller).poll(schedule, SUBJECT, PREDICATE);
+                will(returnValue(false));
+            }});
+
+            String message = messageThrownBy(() -> expressions.when(schedule, SUBJECT, PREDICATE));
+
+            assertThat(message, is(Diagnosis.of(schedule, SUBJECT, PREDICATE)));
         }
     }
 }
