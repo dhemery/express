1a3239ef8d7dfb6504a31d79e8cade1eac301d3b
Dale Emery <dale@dhemery.com>
2017-09-30 17:38:13 -0700

    Convert more tests to JUnit 5

--- src/test/java/com/dhemery/expressions/SubjectFunctionPredicateExpressionTests.java
+++ src/test/java/com/dhemery/expressions/SubjectFunctionPredicateExpressionTests.java
@@ -2,39 +2,39 @@ package com.dhemery.expressions;
 
 import com.dhemery.expressions.diagnosing.Diagnosis;
 import com.dhemery.expressions.diagnosing.Named;
-import com.dhemery.expressions.helpers.Throwables;
-import org.junit.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SubjectFunctionPredicateExpressionTests {
     private static final String SUBJECT = "subject";
     private static final Function<String, String> FUNCTION = Named.function("function", String::toUpperCase);
     private static final Predicate<String> ANY_VALUE = Named.predicate("any value", t -> true);
 
+    @Nested
     public static class AssertThat {
         @Test
         public void returnsWithoutThrowing_ifPredicateAcceptsFunctionOfSubject() {
             Expressions.assertThat(SUBJECT, FUNCTION, ANY_VALUE);
         }
 
-        @Test(expected = AssertionError.class)
-        public void throwsAssertionError_ifPredicateRejectsFunctionOfSubject() {
-            Expressions.assertThat(SUBJECT, FUNCTION, ANY_VALUE.negate());
-        }
-
         @Test
-        public void errorMessage_describesSubjectFunctionPredicateAndDerivedValue() {
-            String message = Throwables.messageThrownBy(() -> Expressions.assertThat(SUBJECT, FUNCTION, ANY_VALUE.negate()));
-
-            assertThat(message, is(Diagnosis.of(SUBJECT, FUNCTION, ANY_VALUE.negate(), FUNCTION.apply(SUBJECT))));
+        public void throwsAssertionError_ifPredicateRejectsFunctionOfSubject() {
+            AssertionError thrown = assertThrows(
+                    AssertionError.class,
+                    () -> Expressions.assertThat(SUBJECT, FUNCTION, ANY_VALUE.negate())
+            );
+            assertThat(thrown.getMessage(), is(Diagnosis.of(SUBJECT, FUNCTION, ANY_VALUE.negate(), FUNCTION.apply(SUBJECT))));
         }
     }
 
+    @Nested
     public static class SatisfiedThat {
         @Test
         public void returnsTrue_ifPredicateAcceptsFunctionOfSubject() {
