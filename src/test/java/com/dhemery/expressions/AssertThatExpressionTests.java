b1bc7b382867e0a2b26efb72aff1665d0cdfbcd0
Dale Emery <dale@dhemery.com>
2017-10-02 13:24:42 -0700

    Move subject function predicate tests to new classes

--- src/test/java/com/dhemery/expressions/AssertThatExpressionTests.java
+++ src/test/java/com/dhemery/expressions/AssertThatExpressionTests.java
@@ -4,7 +4,9 @@ import com.dhemery.expressions.diagnosing.Diagnosis;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Objects;
 import java.util.function.BooleanSupplier;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -12,6 +14,10 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Expressions.assertThat")
 class AssertThatExpressionTests {
+    private static final Object NON_NULL_OBJECT = new Object();
+    private static final Predicate<Object> IS_NULL = Objects::isNull;
+    private static final Predicate<Object> IS_NON_NULL = Objects::nonNull;
+    private static final Function<Object, Object> IDENTITY = Function.identity();
 
     @Test
     void returnsIfSupplierSuppliesTrue() {
@@ -32,19 +38,31 @@ class AssertThatExpressionTests {
 
     @Test
     void returnsIfPredicateAcceptsSubject() {
-        Expressions.assertThat("foo", s -> true);
+        Expressions.assertThat(NON_NULL_OBJECT, IS_NON_NULL);
     }
 
     @Test
     void throwsDiagnosticAssertionErrorIfPredicateRejectsSubject() {
-        String subject = "subject";
-        Predicate<String> rejected = s -> false;
 
         AssertionError thrown = assertThrows(
                 AssertionError.class,
-                () -> Expressions.assertThat(subject, rejected)
+                () -> Expressions.assertThat(NON_NULL_OBJECT, IS_NULL)
         );
 
-        assertEquals(Diagnosis.of(subject, rejected), thrown.getMessage());
+        assertEquals(Diagnosis.of(NON_NULL_OBJECT, IS_NULL), thrown.getMessage());
+    }
+
+    @Test
+    void returnsIfPredicateAcceptsFunctionOfSubject() {
+        Expressions.assertThat(NON_NULL_OBJECT, IDENTITY, IS_NON_NULL);
+    }
+
+    @Test
+    void throwsDiagnosticAssertionErrorIfPredicateRejectsFunctionOfSubject() {
+        AssertionError thrown = assertThrows(
+                AssertionError.class,
+                () -> Expressions.assertThat(NON_NULL_OBJECT, IDENTITY, IS_NULL)
+        );
+        assertEquals(Diagnosis.of(NON_NULL_OBJECT, IDENTITY, IS_NULL, IDENTITY.apply(NON_NULL_OBJECT)), thrown.getMessage());
     }
 }
